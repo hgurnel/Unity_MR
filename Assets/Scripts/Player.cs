e7c51e87a57162ad83a3a45c7a7b156b4587b4ad
hadrien gurnel <hgurnel@gmail.com>
2022-03-16 23:30:32 +0100

    Reset player to 0.5m in front of the camera when pressing space bar

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,72 +7,72 @@ public class Player : MonoBehaviour
     // ----- ATTRIBUTES -----
 
     private Color initColorPlayer;
-    private Transform initTransformPlayer;
+    private Vector3 initScalePlayer;
     private Vector3 currentPosPlayer;
 
-    private Vector3 currentPosCamera;
+    private Transform currentTransformCamera;
 
-    // ----- METHODS -----
+
+
+    // ----- UNITY METHODS -----
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get player initial (color, transform) for later, when we press the button to reset its configuration
+        // Get player initial color and scale for later, when we press the button to reset its configuration
         initColorPlayer = gameObject.GetComponent<MeshRenderer>().material.color;
-        initTransformPlayer = transform;       
+        initScalePlayer = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentPosPlayer = transform.position;
+        currentTransformCamera = GameObject.Find("Main Camera").GetComponent<Camera>().transform;
+
+        if (Input.GetKeyDown("space"))
+            resetPlayer(currentTransformCamera);
+        
         // Change color of Player based on distance to camera
-        float distance = DistancePlayerCam();
+        float distance = Vector3.Distance(currentPosPlayer, currentTransformCamera.position);
         gameObject.GetComponent<MeshRenderer>().material.color = TestDistanceForColor(distance);
     }
 
-    float DistancePlayerCam()
-    {
-        currentPosPlayer = transform.position;
-        currentPosCamera = GameObject.Find("Main Camera").GetComponent<Camera>().transform.position;
-
-        float distance = Vector3.Distance(currentPosPlayer, currentPosCamera);
-
-        Debug.Log("Distance = " + distance);
-
-        return distance;
-    }
+    // ----- OTHER METHODS -----
 
     Color TestDistanceForColor(float distance)
     {
         Color outputColor;
 
         if (distance >= 1.0f && distance < 2.0f)
-        {
-            Debug.Log("Green");
             outputColor = Color.green;
-        }
+
         else if (distance >= 2.0f && distance < 3.0f)
-        {
-            Debug.Log("Orange");
             outputColor = new Color32(255, 165, 0, 255);
-        }
+
         else if (distance >= 3.0f)
-        {
-            Debug.Log("Red");
             outputColor = Color.red;
-        }
+
         else
-        {
-            Debug.Log("Init color");
             outputColor = initColorPlayer;
-        }
 
         return outputColor;
     }
 
-    void resetPlayerPosition()
+    // Places the Player 0.5m in front of the camera, regardless of the orientation of the camera
+    void resetPlayer(Transform camTransform)
     {
+        // Reset player scale
+        transform.localScale = initScalePlayer;
 
+        // Set player position to camera position
+        transform.position = camTransform.position;
+
+        // Set player orientation to camera orientation
+        transform.rotation = camTransform.rotation;
+
+        // Move player by 0.5m along its forward vector
+        transform.Translate(0, 0, 0.5f);
     }
 
 } // class
